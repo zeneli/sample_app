76837ee28a43f4a6e31b4c9ebcdc8429d0181912
zeneli <tud27246@temple.edu>
2016-08-24 14:45:19 -0400

    Change tabs to spaces (2 spaces per tabe)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,10 @@ class StaticPagesController < ApplicationController
   def help
   end
 
-	def about
-	end
+  def about
+  end
 
-	def contact
-	end
+  def contact
+  end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
-		assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 
 end
