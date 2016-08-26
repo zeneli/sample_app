bfec8504c09efce0756709d35394ec7dd64e835a
Amarildo Zeneli <ledozeneli@gmail.com>
2016-08-26 13:57:51 -0400

    Finish signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to user_url(@user)
     else
       render 'new'
     end
