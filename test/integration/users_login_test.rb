f1bb55ffe54446672d21ed137c674c56943ac7fd
zeneli <tud27246@temple.edu>
2016-09-07 17:45:51 -0400

    Add fixture, user digest function, and integration test for valid login flow

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:foo)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -13,4 +14,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
