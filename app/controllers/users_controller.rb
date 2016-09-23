041de051bc2bb56a74ba2a5fd554781f706d01be
zeneli <tud27246@temple.edu>
2016-09-22 21:31:41 -0400

    Pass, user can not update to be admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,7 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation,
-                                   :admin)
+                                   :password_confirmation)
     end
 
     # Before filters
