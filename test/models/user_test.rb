140c7e6e03f3697c01d2e548f56a2572e858216e
zeneli <tud27246@temple.edu>
2016-09-24 15:14:09 -0400

    Idiomatic Micropost test setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   def setup		# create a valid user
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
