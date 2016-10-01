4877e54b594e7b87bca0da53314508045c122aa1
zeneli <tud27246@temple.edu>
2016-10-01 14:03:42 -0400

    Seed microposts and fix micropost view/pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ User.create!(name: "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
+
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -19,4 +20,10 @@ User.create!(name: "Example User",
                password: password,
                password_confirmation: password,
                admin: false)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
