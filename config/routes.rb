2f9ed823dc202ab1d78a7cfdad7d1d4f560cd200
zeneli <tud27246@temple.edu>
2016-10-02 13:30:11 -0400

    Add micropost routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resource :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
