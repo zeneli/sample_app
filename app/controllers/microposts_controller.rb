b22d37f521666514da923c53c45ed30adc1a691c
zeneli <tud27246@temple.edu>
2016-10-02 15:11:05 -0400

    Fix invalid submissions redirect of proto-feed from home-page.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,6 +6,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
