c7d68cf6669544f2dde4b856131534faa10809a7
Amarildo Zeneli <tud27246@temple.edu>
2016-11-22 09:27:49 -0500

    Micropost test order should be most recent first (pass)

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -9,11 +9,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @micropost.valid?
   end
-  test "usser id should be present" do
+  test "user id should be present" do
     @micropost.user_id = nil
     assert_not @micropost.valid?
   end
-
   test "content should be present" do
     @micropost.content = "    "
     assert_not @micropost.valid?
@@ -21,5 +20,8 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end
