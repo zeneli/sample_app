6dbaac314adfee878c37f5c85e5b456f67a99aef
Amarildo Zeneli <tud27246@temple.edu>
2016-11-22 09:36:20 -0500

    User test associated microposts should be destroyed (pass)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,7 +83,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "associated microposts should be destroyed" do
     @user.save
-    @user.microposts.create(content: "Lorem ipsum")
+    @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
