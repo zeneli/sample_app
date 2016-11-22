ccc5919792f240a39011113ed0dbfb2afbdddbe0
Amarildo Zeneli <tud27246@temple.edu>
2016-11-22 09:35:33 -0500

    User test associated microposts should be destroyed (fail)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,4 +80,12 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
