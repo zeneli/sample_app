19dbc0d98e2375a3c7deeb51e63a4d2d1cb13b94
zeneli <tud27246@temple.edu>
2016-11-29 13:21:47 -0500

    Add relationship folower/followed id test

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -8,4 +8,12 @@ class RelationshipTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @relationship.valid?
   end
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
