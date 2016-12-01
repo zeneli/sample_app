99fc4fb6a36e87e4fe9624f4a0fb77063d1e3af1
zeneli <tud27246@temple.edu>
2016-12-01 12:30:51 -0500

    Add routes for User following and followers

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   get  '/login',    to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resource :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
