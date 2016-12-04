78ddfecb4ca4d949403ef736b7b4fa3668cba07e
Amarildo Zeneli <tud27246@temple.edu>
2016-12-04 17:10:41 -0500

    Bug fix- user should be @user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,13 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
+    @user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user
   end
 
   def destroy
-    user = Relationships.find(params[:id]).followed
+    @user = Relationships.find(params[:id]).followed
     current_user.unfollow(user)
     redirect_to user
   end
