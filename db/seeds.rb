e514f3e0281416766d3a62a925f5e3db3e0f0d39
Amarildo Zeneli <tud27246@temple.edu>
2016-12-04 15:45:20 -0500

    No test errors. Begin following/followers page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,27 +11,28 @@ User.create(name: "Foo Bar",
              password_confirmation: "foobar",
              admin: true)
 
-55.times do |n|
-  name = Faker::Name.name
+99.times do |n|
+  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name,
+  User.create!(name:  name,
                email: email,
-               password: password,
+               password:              password,
                password_confirmation: password,
-               admin: false)
+               admin:false)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
-30.times do
+50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
 # Following relationships
 users = User.all
-user = users.first
+user  = users.first
 following = users[2..50]
-followers = users[2..40]
+followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
