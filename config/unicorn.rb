5ee844038d777ce42ff82c8432874114a0e6d4ec
zeneli <tud27246@temple.edu>
2016-12-22 00:30:01 -0500

    Add unicorn config (pass)

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,8 @@
-# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+working_directory "/home/az/workspace/rails/sample_app"
+pid "/home/az/workspace/rails/sample_app/tmp/pids/unicorn.pid"
+stderr_path "/home/az/workspace/rails/sample_app/log/unicorn.log"
+stdout_path "/home/az/workspace/rails/sample_app/log/unicorn.log"
 
-
-# Set unicorn options
+listen 3000
 worker_processes 2
-preload_app true
 timeout 30
-
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
