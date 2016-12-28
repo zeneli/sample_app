a04bc87f480ee34f5ec4f3ee4073624ba2bcd426
Amarildo Zeneli <tud27246@temple.edu>
2016-12-28 09:13:46 -0500

    Fix capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,55 +1,51 @@
 require "bundler/capistrano"
 
-server "192.168.1.171", :web, :app, :db, primary: true 
-# config valid only for current version of Capistrano
-lock "3.7.1"
+server "54.152.116.176", :web, :app, :db, primary: true 
 
 set :application, "sample_app"
-set :user, "az"
-set :deploy_to "/home/#{user}workspace/rails/#{application}"
+set :user, "deployer"
+set :deploy_to "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repo_url, "https://github.com/zeneli/sample_app.git"
-set :branch, "capistrano-playground"
+set :repo_url, "git@github.com:zeneli/sample_app.git"
+set :branch, "master"
 
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-set :pty, true
+default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
-    task comman, roles: :app, except: {no_release: true} do
+    task command, roles: :app, except: {no_release: true} do
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    #run "mkdir -p #{shared_path}/config"
+    #put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    #puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  #task :symlink_config, roles: :app do
+  #  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  #end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
